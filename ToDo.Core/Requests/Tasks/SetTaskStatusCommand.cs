3caf6960294fee7fdba7a59044639e40967ac445
Keskil <kezoneko@gmail.com>
2020-04-15 09:15:06 +0900

    added some code
    added class SetTaskStatusCommand, added validation rules, start doing handler SetTaskStatusCommandHandler

---
+++ ToDo.Core/Requests/Tasks/SetTaskStatusCommand.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ToDo.Core.Enums;
+using MediatR;
+
+namespace ToDo.Core.Requests.Tasks
+{
+    public class SetTaskStatusCommand: IRequest
+    {
+        public int TaskId { get; set; }
+        public Status Status { get; set; }
+    }
+
+    // Validation Rules
+    public class TaskCommandValidator : AbstractValidator<SetTaskStatusCommand>
+    {
+        public TaskCommandValidator()
+        {
+            // TaskId is required, not equal to Guid.Empty = NotEmpty()
+            RuleFor(x => x.TaskId).NotEmpty();
+            // Status is Status
+            RuleFor(x => x.Status).NotNull();
+        }
+    }
+}
---
+++ ToDo.Core/Requests/Tasks/SetTaskStatusCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using MediatR;
+using AutoMapper;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Cynosura.Core.Data;
+using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Text;
+using Task = ToDo.Core.Entities.Task;
+
+namespace ToDo.Core.Requests.Tasks
+{
+    public class SetTaskStatusCommandHandler: IRequestHandler<SetTaskStatusCommand>
+    {
+        private readonly IEntityRepository<Task> _taskRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public SetTaskStatusCommandHandler(IEntityRepository<Task> taskRepository,
+            IUnitOfWork unitOfWork,
+            IMapper mapper)
+        {
+            _taskRepository = taskRepository;
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(SetTaskStatusCommand request, CancellationToken cancellationToken)
+        {
+            var task = await _taskRepository.GetEntities()
+                .Where(e => e.Id == request.TaskId)
+                .FirstOrDefaultAsync();
+            if (task != null)
+            {
+                _mapper.Map(request, task);
+                await _unitOfWork.CommitAsync();
+            }
+            return Unit.Value;
+        }
+    }
+}
